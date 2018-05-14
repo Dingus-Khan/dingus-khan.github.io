7aa449f04e9f89cef81d21a429b40858e42363f2
m.giff <m.giff@sterlingbuild.co.uk>
2018-05-14 14:33:11 +0100

    asd

--- testbed.js
+++ testbed.js
@@ -205,7 +205,7 @@ var canvas = document.getElementById("main");
 var gl = canvas.getContext("webgl2");
 
 gl.enable(gl.BLEND);
-gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+gl.blendFunc(gl.ONE_MINUS_SRC_ALPHA, gl.SRC_ALPHA);
 
 gl.enable(gl.DEPTH_TEST);
 gl.depthFunc(gl.LESS);
