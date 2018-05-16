1b739905b406c18cda1f1f29b4827ddbf0d3dbc1
Micheal Giff <Micheal.Giff@gmail.com>
2018-05-16 23:46:21 +0100

    asd

--- testbed.js
+++ testbed.js
@@ -120,6 +120,8 @@ function run() {
 	} else {
 		tile.x = Mouse.x - Mouse.x % tile.tw;
 		tile.y = Mouse.y - Mouse.y % tile.th;
+		tileSetOverlay.draw();
+		selectionSprite.draw();
 	}
 	requestAnimationFrame(run);
 }
