d6513281fd910f30a5ae0606800c953057d1f70b
m.giff <m.giff@sterlingbuild.co.uk>
2018-05-18 13:42:59 +0100

    asd

---
+++ collision.js
@@ -0,0 +1,62 @@
+var vertexShader = `#version 300 es
+in vec2 pos;
+in vec2 tex;
+
+out vec2 Tex;
+
+uniform vec2 texSize;
+uniform mat4 proj;
+uniform mat4 model;
+
+void main(){
+    Tex = tex / texSize;
+    gl_Position = proj * model * vec4(pos, 0.0, 1.0);
+}`;
+
+var fragmentShader = `#version 300 es
+precision mediump float;
+
+in vec2 Tex;
+
+out vec4 outColour;
+
+uniform sampler2D texImage;
+
+void main(){
+    outColour = texture(texImage, Tex);
+}`;
+
+Keyboard.registerKey('left', 37);
+Keyboard.registerKey('up', 38);
+Keyboard.registerKey('right', 39);
+Keyboard.registerKey('down', 40);
+Keyboard.registerKey('space', 32);
+Keyboard.registerKey('a', 65);
+Keyboard.registerKey('d', 68);
+Keyboard.registerKey('s', 83);
+Keyboard.registerKey('w', 87);
+
+var vertex = System.buildShader(gl, gl.VERTEX_SHADER, vertexShader);
+var fragment = System.buildShader(gl, gl.FRAGMENT_SHADER, fragmentShader);
+var program = System.linkProgram(gl, vertex, fragment);
+
+gl.useProgram(program);
+
+var proj = [
+    2 / 800, 0, 0, 0,
+    0, -2 / 600, 0, 0,
+    0, 0, 2 / 1200, 0,
+    -1, 1, 0.5, 1
+];
+
+var projLoc = gl.getUniformLocation(program, "proj");
+gl.uniformMatrix4fv(projLoc, false, proj);
+
+requestAnimationFrame(run);
+
+gl.clearColor(0.1, 0.1, 0.1, 1.0);
+
+function run() {
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	requestAnimationFrame(run);
+}
