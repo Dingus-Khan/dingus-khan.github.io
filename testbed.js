352144aea58cfaa61fca804885985d1f41514530
m.giff <m.giff@sterlingbuild.co.uk>
2018-05-25 12:09:38 +0100

    asd

--- testbed.js
+++ testbed.js
@@ -5,90 +5,13 @@ var ShaderCache = {
 	"til0e": {},
 };
 
-var Matrix = {
-	identity: function(){
-		return [
-			1, 0, 0, 0,
-			0, 1, 0, 0,
-			0, 0, 1, 0,
-			0, 0, 0, 1
-		];
-	},
-	translation: function(x, y){
-		return [
-			1, 0, 0, 0,
-			0, 1, 0, 0,
-			0, 0, 1, 0,
-			Math.round(x), Math,round(y), 0, 1
-		];
-	},
-	translate: function(m, x, y){
-		return this.multiply(m, this.translation(x, y));
-	},
-	rotation: function(r){
-		return [
-			1, 0, 0, 0,
-			0, 1, 0, 0,
-			0, 0, 1, 0,
-			0, 0, 0, 1
-		];
-	},
-	rotate: function(m, r){
-		return this.multiply(m, this.rotation(r));
-	},
-	scaling: function(sx, sy){
-		return [
-			sx, 0, 0, 0,
-			0, sy, 0, 0,
-			0, 0, 1, 0,
-			0, 0, 0, 1
-		];
-	},
-	scale: function(m, s){
-		return this.multiply(m, this.scaling(s));
-	},
-	multiply: function(m1, m2){
-		return [
-			((m1[0] * m2[0]) + (m1[1] * m2[4]) + (m1[2] * m2[8]) + (m1[3] * m2[12])),
-			((m1[0] * m2[1]) + (m1[1] * m2[5]) + (m1[2] * m2[9]) + (m1[3] * m2[13])),
-			((m1[0] * m2[2]) + (m1[1] * m2[6]) + (m1[2] * m2[10]) + (m1[3] * m2[14])),
-			((m1[0] * m2[3]) + (m1[1] * m2[7]) + (m1[2] * m2[11]) + (m1[3] * m2[15])),
-
-			((m1[4] * m2[0]) + (m1[5] * m2[4]) + (m1[6] * m2[8]) + (m1[7] * m2[12])),
-			((m1[4] * m2[1]) + (m1[5] * m2[5]) + (m1[6] * m2[9]) + (m1[7] * m2[13])),
-			((m1[4] * m2[2]) + (m1[5] * m2[6]) + (m1[6] * m2[10]) + (m1[7] * m2[14])),
-			((m1[4] * m2[3]) + (m1[5] * m2[7]) + (m1[6] * m2[11]) + (m1[7] * m2[15])),
-
-			((m1[8] * m2[0]) + (m1[9] * m2[4]) + (m1[10] * m2[8]) + (m1[11] * m2[12])),
-			((m1[8] * m2[1]) + (m1[9] * m2[5]) + (m1[10] * m2[9]) + (m1[11] * m2[13])),
-			((m1[8] * m2[2]) + (m1[9] * m2[6]) + (m1[10] * m2[10]) + (m1[11] * m2[14])),
-			((m1[8] * m2[3]) + (m1[9] * m2[7]) + (m1[10] * m2[11]) + (m1[11] * m2[15])),
-
-			((m1[12] * m2[0]) + (m1[13] * m2[4]) + (m1[14] * m2[8]) + (m1[15] * m2[12])),
-			((m1[12] * m2[1]) + (m1[13] * m2[5]) + (m1[14] * m2[9]) + (m1[15] * m2[13])),
-			((m1[12] * m2[2]) + (m1[13] * m2[6]) + (m1[14] * m2[10]) + (m1[15] * m2[14])),
-			((m1[12] * m2[3]) + (m1[13] * m2[7]) + (m1[14] * m2[11]) + (m1[15] * m2[15]))
-		];
-	}
-};
-
 var Camera = {
 	x: 0,
 	y: 0,
 	r: 0,
 	z: 0,
 	update: true,
-	model: [
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1
-	],
-	pan: function(x, y){ this.x = x; this.y = y; this.update = true; },
-	rotate: function(r){ this.r = r; this.update = true; },
-	zoom: function(z){ this.z = z; this.update = true; },
-	getView: function(){
-	}
+	matrix: Matrix.identity()
 };
 
 requestAnimationFrame(run);
