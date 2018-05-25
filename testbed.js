ab8395943f19d2bd527e053af2444014aab4efb5
m.giff <m.giff@sterlingbuild.co.uk>
2018-05-25 11:53:15 +0100

    asd

--- testbed.js
+++ testbed.js
@@ -1,40 +1,99 @@
-Keyboard.registerKey('left', 37);
-Keyboard.registerKey('up', 38);
-Keyboard.registerKey('right', 39);
-Keyboard.registerKey('down', 40);
-Keyboard.registerKey('space', 32);
-Keyboard.registerKey('a', 65);
-Keyboard.registerKey('d', 68);
-Keyboard.registerKey('s', 83);
-Keyboard.registerKey('w', 87);
+// SHADER CODE
+var ShaderCache = {
+	"sprite": {},
+	"debug": {},
+	"til0e": {},
+};
 
-function checkCollision(a, b){
-	var c1 = {r: a.w / 3, x: a.x + (a.w / 2) + a.vel.x, y: a.y + (a.h / 1.5) + a.vel.y};
-	var c2 = {r: b.w / 3, x: b.x + (b.w / 2) + b.vel.x, y: b.y + (b.h / 1.5) + b.vel.y};
-	var dx = c1.x - c2.x;
-	var dy = c1.y - c2.y;
-	var dist = Math.sqrt(dx * dx + dy * dy);
-	if (dist < c1.r + c2.r){
-		return true;
-	} else {
-		return false;
+var Matrix = {
+	identity: function(){
+		return [
+			1, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1
+		];
+	},
+	translation: function(x, y){
+		return [
+			1, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 0, 1, 0,
+			Math.round(x), Math,round(y), 0, 1
+		];
+	},
+	translate: function(m, x, y){
+		return this.multiply(m, this.translation(x, y));
+	},
+	rotation: function(r){
+		return [
+			1, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1
+		];
+	},
+	rotate: function(m, r){
+		return this.multiply(m, this.rotation(r));
+	},
+	scaling: function(sx, sy){
+		return [
+			sx, 0, 0, 0,
+			0, sy, 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1
+		];
+	},
+	scale: function(m, s){
+		return this.multiply(m, this.scaling(s));
+	},
+	multiply: function(m1, m2){
+		return [
+			((m1[0] * m2[0]) + (m1[1] * m2[4]) + (m1[2] * m2[8]) + (m1[3] * m2[12]),
+			((m1[0] * m2[1]) + (m1[1] * m2[5]) + (m1[2] * m2[9]) + (m1[3] * m2[13]),
+			((m1[0] * m2[2]) + (m1[1] * m2[6]) + (m1[2] * m2[10]) + (m1[3] * m2[14]),
+			((m1[0] * m2[3]) + (m1[1] * m2[7]) + (m1[2] * m2[11]) + (m1[3] * m2[15]),
+
+			((m1[4] * m2[0]) + (m1[5] * m2[4]) + (m1[6] * m2[8]) + (m1[7] * m2[12]),
+			((m1[4] * m2[1]) + (m1[5] * m2[5]) + (m1[6] * m2[9]) + (m1[7] * m2[13]),
+			((m1[4] * m2[2]) + (m1[5] * m2[6]) + (m1[6] * m2[10]) + (m1[7] * m2[14]),
+			((m1[4] * m2[3]) + (m1[5] * m2[7]) + (m1[6] * m2[11]) + (m1[7] * m2[15]),
+
+			((m1[8] * m2[0]) + (m1[9] * m2[4]) + (m1[10] * m2[8]) + (m1[11] * m2[12]),
+			((m1[8] * m2[1]) + (m1[9] * m2[5]) + (m1[10] * m2[9]) + (m1[11] * m2[13]),
+			((m1[8] * m2[2]) + (m1[9] * m2[6]) + (m1[10] * m2[10]) + (m1[11] * m2[14]),
+			((m1[8] * m2[3]) + (m1[9] * m2[7]) + (m1[10] * m2[11]) + (m1[11] * m2[15]),
+
+			((m1[12] * m2[0]) + (m1[13] * m2[4]) + (m1[14] * m2[8]) + (m1[15] * m2[12]),
+			((m1[12] * m2[1]) + (m1[13] * m2[5]) + (m1[14] * m2[9]) + (m1[15] * m2[13]),
+			((m1[12] * m2[2]) + (m1[13] * m2[6]) + (m1[14] * m2[10]) + (m1[15] * m2[14]),
+			((m1[12] * m2[3]) + (m1[13] * m2[7]) + (m1[14] * m2[11]) + (m1[15] * m2[15])
+		];
 	}
-}
+};
 
-var tileSet = new Texture("tileset.png");
-TileBatch.init(tileSet);
-
-var sprite = new Texture("test.png");
-var spr = new Sprite(0, 0, 120, 120, 0, 0, 120, 120, sprite);
-spr.addAnimation(new Animation(0, 5, 0, 120, 120, 5));
-SpriteList.sprites[0] = spr;
+var Camera = {
+	x: 0,
+	y: 0,
+	r: 0,
+	z: 0,
+	update: true,
+	model: [
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+	],
+	pan: function(x, y){ this.x = x; this.y = y; this.update = true; },
+	rotate: function(r){ this.r = r; this.update = true; },
+	zoom: function(z){ this.z = z; this.update = true; },
+	getView: function(){
+	}
+};
 
 requestAnimationFrame(run);
 function run() {
-	spr.update();
-
+	Keyboard.update();
 	gl.clear(gl.COLOR_BUFFER_BIT);
-	TileBatch.draw();
-	SpriteList.draw();
 	requestAnimationFrame(run);
 }
