71cfb4fa318f9c3c2fd04f209e6e4c9bb403dea0
m.giff <m.giff@sterlingbuild.co.uk>
2018-05-25 14:42:46 +0100

    asd

--- testbed.js
+++ testbed.js
@@ -8,33 +8,39 @@ var ShaderCache = {
 var Camera = {
 	x: 0,
 	y: 0,
+	trauma: 0,
 	r: 0,
 	z: 1,
-	update: true,
+	updateMatrix: true,
 	matrix: Matrix.identity(),
 	panTo: function(x, y){
 		this.x = -x;
 		this.y = -y;
-		this.update = true;
+		this.updateMatrix = true;
 	},
 	rotateTo: function(r){
 		this.r = -r;
-		this.update = true;
+		this.updateMatrix = true;
 	},
 	zoom: function(z){
 		this.z = z;
-		this.update = true;
+		this.updateMatrix = true;
 	},
 	getMatrix: function(){
-		if(this.update){
+		if(this.updateMatrix){
 			this.matrix = Matrix.identity();
 			this.matrix = Matrix.translate(this.matrix, this.x, this.y);
 			this.matrix = Matrix.rotate(this.matrix, this.r);
 			this.matrix = Matrix.scale(this.matrix, this.z, this.z);
-			this.update = false;
+			this.updateMatrix = false;
 		}
 		return this.matrix;
 	},
+	update: function(){
+		if (trauma > 0){
+			trauma--;
+		}
+	}
 };
 
 var spritesheet = new Texture("test.png");
