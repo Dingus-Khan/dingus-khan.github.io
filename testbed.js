f289e734ed4b1f9633fdebf2d5f23d688ac2df74
m.giff <m.giff@sterlingbuild.co.uk>
2018-05-25 12:32:19 +0100

    asd

--- testbed.js
+++ testbed.js
@@ -11,7 +11,30 @@ var Camera = {
 	r: 0,
 	z: 0,
 	update: true,
-	matrix: Matrix.identity()
+	matrix: Matrix.identity(),
+	pan: function(x, y){
+		this.x = -x;
+		this.y = -y;
+		this.update = true;
+	},
+	rotate: function(r){
+		this.r = -r;
+		this.update = true;
+	},
+	zoom: function(z){
+		this.z = z;
+		this.update = true;
+	},
+	getMatrix: function(){
+		if(this.update){
+			this.matrix = Matrix.identity();
+			this.matrix = Matrix.translate(this.matrix, this.x, this.y);
+			this.matrix = Matrix.rotate(this.matrix, this.r);
+			this.matrix = Matrix.scale(this.matrix, this.z);
+			this.update = false;
+		}
+		return this.matrix;
+	},
 };
 
 requestAnimationFrame(run);
