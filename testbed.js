d9051c6ac92d9658a7582e89c920edbc0915e2ac
m.giff <m.giff@sterlingbuild.co.uk>
2018-05-29 16:49:44 +0100

    Cleanup

--- testbed.js
+++ testbed.js
@@ -1,29 +1,5 @@
-// SHADER CODE
-var ShaderCache = {
-	"sprite": {},
-	"debug": {},
-	"tile": {},
-};
-
-
-
-var spritesheet = new Texture("test.png");
-var sprite = new Sprite(0, 0, 120, 120, 0, 0, 120, 120, spritesheet, 1.0, 1.0, 1.0);
-sprite.move(370, 270);
-
-var tileSet = new Texture("tileset.png");
-TileBatch.init(tileSet);
-
-
-
 requestAnimationFrame(run);
 function run() {
-	Camera.x = (sprite.x - (sprite.w / 2));
-	Camera.y = (sprite.y - (sprite.h / 2));
-
-	Camera.update();
-	Keyboard.update();
 	gl.clear(gl.COLOR_BUFFER_BIT);
-	sprite.draw();
 	requestAnimationFrame(run);
 }
