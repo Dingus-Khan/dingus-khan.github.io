1400b19146309d2259e65728e65f160fd6c93972
m.giff <m.giff@sterlingbuild.co.uk>
2018-05-31 14:13:38 +0100

    asd

--- testbed.js
+++ testbed.js
@@ -1,22 +1,13 @@
 var vertexShader = `#version 300 es
 in vec2 pos;
-in vec3 col;
-out vec3 Col;
-
 uniform mat4 proj;
-
 void main(){
-	Col = col;
     gl_Position = proj * vec4(pos, 0.0, 1.0);
 }`;
 
 var fragmentShader = `#version 300 es
 precision mediump float;
-
-in vec3 Col;
-
 out vec4 outColour;
-
 void main(){
     outColour = vec4(Col, 1.0);
 }`;
