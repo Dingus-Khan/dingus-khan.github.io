17da6daeea4f4c81c01b80ad0b725e7f827e4157
m.giff <m.giff@sterlingbuild.co.uk>
2018-05-31 15:00:46 +0100

    asd

--- testbed.js
+++ testbed.js
@@ -18,23 +18,18 @@ shader.use();
 
 shader.setUniform("proj", [2 / 800, 0, 0, 0, 0, -2 / 600, 0, 0, 0, 0, 1, 0, -1, 1, 0, 1]);
 
-var drawable = new Drawable(gl.TRIANGLE_FAN, 0);
-
-var offset = 0;
-var scale = 1;
-
-drawable.bufferData = [];
-for(i = 0; i < 600; i += 2){
-	for(j = 0; j < 801; j++){
-		offset += (-0.5 + Math.random());
-		drawable.bufferData.push(j);
-		drawable.bufferData.push(i + (offset * scale));
-	}
-}
-drawable.vertexCount = drawable.bufferData.length / 2;
+var drawable = new Drawable(gl.TRIANGLES, 0);
 
 requestAnimationFrame(run);
 function run() {
+	drawable.bufferData = [
+		Math.abs(Math.sin(performance.now()) + (Math.random())), Math.abs(Math.sin(performance.now()) + (Math.random() * 100)),
+		Math.abs(Math.sin(performance.now()) + (Math.random() * 100)), Math.abs(Math.sin(performance.now()) + (Math.random() * 100)),
+		Math.abs(Math.sin(performance.now()) + (Math.random() * 100)), Math.abs(Math.sin(performance.now()) + (Math.random() * 100))
+	];
+
+	drawable.vertexCount = drawable.bufferData.length / 2;
+	drawable.updateBuffer = true;
 
 	Clear();
 	drawable.draw(shader);
