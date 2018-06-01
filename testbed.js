c798cf7c0b0cb5628460efcde948f857787a56ae
m.giff <m.giff@sterlingbuild.co.uk>
2018-06-01 16:36:22 +0100

    asd

--- testbed.js
+++ testbed.js
@@ -27,43 +27,14 @@ shader.setUniform("proj", [2 / 800, 0, 0, 0, 0, -2 / 600, 0, 0, 0, 0, 1, 0, -1, 
 
 var drawable = new Drawable(gl.TRIANGLES, 0);
 
-var x = 0;
-var y = 0;
-
 drawable.bufferData = [
-	x, y, 1, 1, 1, 1,
-	x + 50, y + 50, 1, 1, 1, 1,
-	x, y + 100, 1, 1, 1, 1,
 ];
 
 drawable.vertexCount = drawable.bufferData.length / 6;
 drawable.updateBuffer = true;
 
-var t = 0;
-var vel = 50;
-var dir = 1;
-
-Keyboard.registerKey("right", 39);
-Keyboard.registerKey("left", 37);
-
 requestAnimationFrame(run);
 function run() {
-	vel += 1;
-
-	x += vel;
-
-	drawable.bufferData = [
-		x, y, 1, 0, 0, 1,
-		x - (vel * 100), y + 50, 1, 0, 0, 1,
-		x, y + 100, 1, 0, 0, 1,
-		x, y, 1, 1, 1, 1,
-		x + (50 * dir), y + 50, 1, 1, 1, 1,
-		x, y + 100, 1, 1, 1, 1,
-	];
-
-	drawable.vertexCount = drawable.bufferData.length / 6;
-	drawable.updateBuffer = true;
-
 	Clear();
 	drawable.draw(shader);
 	requestAnimationFrame(run);
