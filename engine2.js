23f0ae159c2fa5b83ffbcef510c7eadf3b6c0774
Micheal Giff <Micheal.Giff@gmail.com>
2018-06-06 19:31:46 +0100

    asd

--- engine2.js
+++ engine2.js
@@ -33,6 +33,9 @@ var System = {
 
 function Init(){
 	gl.clearColor(0.2, 0.2, 0.2, 1.0);
+
+	gl.enable(gl.BLEND);
+	gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 }
 
 function Clear(){
@@ -48,15 +51,17 @@ function Shader(vertexShader, fragmentShader){
 
 	this.addAttribute = function(name, count, type, normalise, stride, offset){
 		var loc = gl.getAttribLocation(this.shader, name);
-		this.attributes.push({
-			loc: loc, count: count, type: type, normalise: normalise, stride: stride, offset: offset
-		});
+		if (loc != -1){
+			this.attributes.push({
+				loc: loc, count: count, type: type, normalise: normalise, stride: stride, offset: offset
+			});
+		}
 	}
 
 	this.enableAttributes = function(){
 		for(i = 0; i < this.attributes.length; i++){
 			var attr = this.attributes[i];
-			gl.vertexAttribPointer(attr.loc, attr.count, attr.type, attr.normalise, attr.stride, attr.offset);
+			gl.vertexAttribPointer(attr.loc, attr.count, attr.type, attr.normalise, attr.stride * 4, attr.offset * 4);
 			gl.enableVertexAttribArray(attr.loc);
 		}
 	}
@@ -104,12 +109,16 @@ function Drawable(drawMode, vertexCount){
 		}
 	}
 
-	this.draw = function(shader){
+	this.draw = function(shader, texture){
 		if (this.vertexCount > 0){
 			gl.bindVertexArray(this.vao);
 			gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
 
 			this.build();
+
+			if(texture != undefined){
+				texture.bind();
+			}
 
 			shader.enableAttributes();
 
@@ -118,8 +127,105 @@ function Drawable(drawMode, vertexCount){
 	}
 }
 
-function Sprite(){
-	Drawable.call(this, gl.TRIANGLE_STRIP);
+function Texture(image, wrapMode, filterMode){
+	this.texture = gl.createTexture();
+	this.image = new Image();
+	this.image.textureid = this.texture;
+	this.image.wrapMode = wrapMode || gl.REPEAT;
+	this.image.filterMode = filterMode || gl.NEAREST;
+
+	this.image.onload = function(){
+		gl.bindTexture(gl.TEXTURE_2D, this.textureid);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this);
+
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, this.wrapMode);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, this.wrapMode);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, this.filterMode);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, this.filterMode);
+
+		gl.generateMipmap(gl.TEXTURE_2D);
+	}
+	this.image.src = image;
+
+	this.bind = function(){
+		gl.bindTexture(gl.TEXTURE_2D, this.texture);
+	}
 }
+
+var Matrix = {
+	identity: function(){
+		return [
+			1, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1
+		];
+	},
+	translation: function(x, y){
+		return [
+			1, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 0, 1, 0,
+			Math.round(x), Math.round(y), 0, 1
+		];
+	},
+	translate: function(m, x, y){
+		return this.multiply(m, this.translation(x, y));
+	},
+	rotation: function(r){
+		var sine = Math.sin(r);
+		var cosine = Math.cos(r);
+		return [
+			cosine, -sine, 0, 0,
+			sine, cosine, 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1
+		];
+	},
+	rotate: function(m, r){
+		return this.multiply(m, this.rotation(r));
+	},
+	scaling: function(sx, sy){
+		return [
+			sx, 0, 0, 0,
+			0, sy, 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1
+		];
+	},
+	scale: function(m, sx, sy){
+		return this.multiply(m, this.scaling(sx, sy));
+	},
+	multiply: function(m1, m2){
+		return [
+			((m1[0] * m2[0]) + (m1[1] * m2[4]) + (m1[2] * m2[8]) + (m1[3] * m2[12])),
+			((m1[0] * m2[1]) + (m1[1] * m2[5]) + (m1[2] * m2[9]) + (m1[3] * m2[13])),
+			((m1[0] * m2[2]) + (m1[1] * m2[6]) + (m1[2] * m2[10]) + (m1[3] * m2[14])),
+			((m1[0] * m2[3]) + (m1[1] * m2[7]) + (m1[2] * m2[11]) + (m1[3] * m2[15])),
+
+			((m1[4] * m2[0]) + (m1[5] * m2[4]) + (m1[6] * m2[8]) + (m1[7] * m2[12])),
+			((m1[4] * m2[1]) + (m1[5] * m2[5]) + (m1[6] * m2[9]) + (m1[7] * m2[13])),
+			((m1[4] * m2[2]) + (m1[5] * m2[6]) + (m1[6] * m2[10]) + (m1[7] * m2[14])),
+			((m1[4] * m2[3]) + (m1[5] * m2[7]) + (m1[6] * m2[11]) + (m1[7] * m2[15])),
+
+			((m1[8] * m2[0]) + (m1[9] * m2[4]) + (m1[10] * m2[8]) + (m1[11] * m2[12])),
+			((m1[8] * m2[1]) + (m1[9] * m2[5]) + (m1[10] * m2[9]) + (m1[11] * m2[13])),
+			((m1[8] * m2[2]) + (m1[9] * m2[6]) + (m1[10] * m2[10]) + (m1[11] * m2[14])),
+			((m1[8] * m2[3]) + (m1[9] * m2[7]) + (m1[10] * m2[11]) + (m1[11] * m2[15])),
+
+			((m1[12] * m2[0]) + (m1[13] * m2[4]) + (m1[14] * m2[8]) + (m1[15] * m2[12])),
+			((m1[12] * m2[1]) + (m1[13] * m2[5]) + (m1[14] * m2[9]) + (m1[15] * m2[13])),
+			((m1[12] * m2[2]) + (m1[13] * m2[6]) + (m1[14] * m2[10]) + (m1[15] * m2[14])),
+			((m1[12] * m2[3]) + (m1[13] * m2[7]) + (m1[14] * m2[11]) + (m1[15] * m2[15]))
+		];
+	}
+};
+
+function Sprite(texture){
+	Drawable.call(this, gl.TRIANGLE_STRIP);
+	this.texture = texture;
+}
+
+
 
 Init();
--- testbed.js
+++ testbed.js
@@ -1,37 +1,55 @@
 var vertexShader = `#version 300 es
 in vec2 pos;
+in vec2 tex;
+
+out vec2 Tex;
+
 uniform mat4 proj;
+
+uniform vec2 texSize;
+
 void main(){
+	Tex = tex / texSize;
     gl_Position = proj * vec4(pos, 0.0, 1.0);
 }`;
 
 var fragmentShader = `#version 300 es
 precision mediump float;
 out vec4 outColour;
+
+in vec2 Tex;
+
+uniform sampler2D texImage;
+
 void main(){
-    outColour = vec4(1.0);
+    outColour = texture(texImage, Tex);
 }`;
 
 var shader = new Shader(vertexShader, fragmentShader);
-shader.addAttribute("pos", 2, gl.FLOAT, false, 0, 0);
+shader.addAttribute("pos", 2, gl.FLOAT, false, 4, 0);
+shader.addAttribute("tex", 2, gl.FLOAT, false, 4, 2);
 shader.use();
 
 shader.setUniform("proj", [2 / 800, 0, 0, 0, 0, -2 / 600, 0, 0, 0, 0, 1, 0, -1, 1, 0, 1]);
 
-var drawable = new Drawable(gl.TRIANGLES, 0);
+var drawable = new Drawable(gl.TRIANGLE_STRIP, 0);
+
+drawable.bufferData = [
+	0, 0, 0, 0,
+	120, 0, 120, 0,
+	0, 120, 0, 120,
+	120, 120, 120, 120
+];
+
+drawable.vertexCount = drawable.bufferData.length / 4;
+
+var t = new Texture("test.png");
+
+shader.setUniform("texSize", [ t.image.width, t.image.height ]);
 
 requestAnimationFrame(run);
 function run() {
-	drawable.bufferData = [
-		Math.abs(Math.sin(performance.now()) + (Math.random())), Math.abs(Math.sin(performance.now()) + (Math.random() * 100)),
-		Math.abs(Math.sin(performance.now()) + (Math.random() * 100)), Math.abs(Math.sin(performance.now()) + (Math.random() * 100)),
-		Math.abs(Math.sin(performance.now()) + (Math.random() * 100)), Math.abs(Math.sin(performance.now()) + (Math.random() * 100))
-	];
-
-	drawable.vertexCount = drawable.bufferData.length / 2;
-	drawable.updateBuffer = true;
-
 	Clear();
-	drawable.draw(shader);
+	drawable.draw(shader, t);
 	requestAnimationFrame(run);
 }
