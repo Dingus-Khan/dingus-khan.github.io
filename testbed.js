c9cd578892fba2c7a7fd623aa715db55ab85a998
m.giff <m.giff@sterlingbuild.co.uk>
2018-06-15 14:11:14 +0100

    asd

--- testbed.js
+++ testbed.js
@@ -124,7 +124,7 @@ var IdleState = {
 var WalkState = {
 	update: function(drawable){
 		this.ticks++;
-		if (this.ticks > (this.anim.time - ((this.anim.time * Keyboard.getKey('lshift')) / 4))){
+		if (this.ticks > (this.anim.time - (this.anim.time * Keyboard.getKey('lshift')) / 4)){
 			this.frame++;
 			this.ticks = 0;
 
