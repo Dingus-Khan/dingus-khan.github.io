b6f94a28591b35da30ee2ca60baf6a903552c2dd
m.giff <m.giff@sterlingbuild.co.uk>
2018-06-26 11:21:00 +0100

    asd

--- proceduralterrain.js
+++ proceduralterrain.js
@@ -31,49 +31,22 @@ shader.use();
 shader.setUniform("proj", proj);
 
 var Terrain = function(){
-	Drawable.call(this, gl.TRIANGLE_STRIP, 4);
+	Drawable.call(this, gl.TRIANGLE_STRIP, 0);
 
-	this.bufferData = [
-	];
-
-	var width = 17;
-	var height = 17;
-	for(r = 0; r < width; r++){
-		for(c = 0; c < height; c++){
-			this.bufferData.push(r * 30, c * 30, Math.random(), Math.random(), Math.random());
-		}
-	}
-
-	this.vertexCount = this.bufferData.length / 5;
+	this.bufferData = [];
 }
 
 var c = new Terrain();
-var x = 100;
-var y = 100;
-var z = 0;
-var xInc = 100;
 
 requestAnimationFrame(run);
 function run() {
 
 	if (Keyboard.wasKeyPressed('right')){
-		if (z == 0){
-			x += xInc;
-			y -= 100;
-			z = 1;
-		} else {
-			y += 100;
-			z = 0;
-		}
 
-		c.bufferData.push(x, y, Math.random(), Math.random(), Math.random());
+		c.bufferData.push(Math.random() * 800, Math.random() * 600, Math.random(), Math.random(), Math.random());
 		c.updateBuffer = true;
 		c.vertexCount = c.bufferData.length / 5;
 
-		if (x > 800){
-			xInc = -xInc;
-			y += 100;
-		}
 	}
 
 	Keyboard.update();
