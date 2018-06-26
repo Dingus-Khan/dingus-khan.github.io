6ec9f8c60d61f701c0aa2a2be4c2376da45ef2df
m.giff <m.giff@sterlingbuild.co.uk>
2018-06-26 13:27:38 +0100

    asd

--- leveleditor.js
+++ leveleditor.js
@@ -29,3 +29,33 @@ void main(){
 	ivec2 texSize = textureSize(texImage, 0);
     outColour = texture(texImage, vec2(Tex.x / float(texSize.x), Tex.y / float(texSize.y))) * Col;
 }`;
+
+var shader = new Shader(vertexShader, fragmentShader);
+shader.addAttribute("pos", 2, gl.FLOAT, false, 7, 0);
+shader.addAttribute("col", 2, gl.FLOAT, false, 7, 2);
+shader.addAttribute("tex", 2, gl.FLOAT, false, 7, 5);
+shader.use();
+
+shader.setUniform("proj", proj);
+
+var Tile = function(x, y, w, h, tx, ty, tw, th){
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+	this.tx = tx;
+	this.ty = ty;
+	this.tw = tw;
+	this.th = th;
+}
+
+var TileTypes = {
+	"Platform": new Tile(0, 0, 100, 100, 0, 0, 100, 100)
+};
+
+requestAnimationFrame(run);
+function run() {
+	Keyboard.update();
+	Clear();
+	requestAnimationFrame(run);
+}
