0e1b2da77e38a00055484b9cad7f0911b9bdae0e
m.giff <m.giff@sterlingbuild.co.uk>
2018-06-28 14:11:26 +0100

    asd

--- shaders.js
+++ shaders.js
@@ -33,6 +33,34 @@ panelShader.setUniform("model", Matrix.identity());
 
 
 
-var buttonVertexShader = `#version 300 else {
-	in vec2 pos
-}`;
+var buttonVertexShader = `#version 300 es
+	in vec2 pos;
+	in vec2 tex;
+	in vec3 col;
+
+	out vec2 Tex;
+	out vec3 Col;
+
+	uniform vec2 texSize;
+	uniform mat4 proj;
+	uniform mat4 model;
+
+	void main(){
+		Tex = tex / texSize;
+		Col = col;
+		gl_Position = proj * model * vec4(pos, 0.0, 1.0);
+	}
+`;
+
+var buttonFragmentShader = `#version 300 es
+	in vec2 Tex;
+	in vec3 Col;
+
+	out vec4 outColour;
+
+	uniform sampler2D texImage;
+
+	void main(){
+		outColour = texture(texImage, Tex) * vec4(Col, 1.0);
+	}
+`;
