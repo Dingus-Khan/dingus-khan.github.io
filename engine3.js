dffc80f330f81b32cc8e17d899821607f65560a6
m.giff <m.giff@sterlingbuild.co.uk>
2018-07-17 08:18:55 +0100

    asd

--- engine3.js
+++ engine3.js
@@ -373,10 +373,6 @@ var Sprite = function(tex){
 	this.vao = gl.createVertexArray();
 	this.vbo = gl.createBuffer();
 	this.bufferData = [
-		0, 0, 0, 0, 1, 1, 1,
-		100, 0, 100, 0, 1, 1, 1,
-		100, 100, 100, 100, 1, 1, 1,
-		0, 100, 0, 100, 1, 1, 1
 	];
 
 	gl.bindVertexArray(this.vao);
