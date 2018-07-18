2a9fd04ec88ff2e776bfb1def4328d36cb047589
m.giff <m.giff@sterlingbuild.co.uk>
2018-07-18 13:25:18 +0100

    asd

--- engine4.js
+++ engine4.js
@@ -538,10 +538,10 @@ class Player extends Animation {
 		this.addAnimation("walk_up", 0, 4, 2, 10, 16, 32, false, 16, 32);
 		this.addAnimation("walk_left", 0, 4, 3, 10, 16, 32, false, 16, 32);
 
-		this.addAnimation("attack_down", 0, 4, 4, 4, 32, 32, false, 32, 32);
-		this.addAnimation("attack_up", 0, 4, 5, 4, 32, 32, false, 32, 32);
-		this.addAnimation("attack_right", 0, 4, 6, 4, 32, 32, false, 32, 32);
-		this.addAnimation("attack_left", 0, 4, 7, 4, 32, 32, false, 32, 32);
+		this.addAnimation("attack_down", 0, 4, 4, 5, 32, 32, false, 32, 32);
+		this.addAnimation("attack_up", 0, 4, 5, 5, 32, 32, false, 32, 32);
+		this.addAnimation("attack_right", 0, 4, 6, 5, 32, 32, false, 32, 32);
+		this.addAnimation("attack_left", 0, 4, 7, 5, 32, 32, false, 32, 32);
 
 		this.setAnimation("idle_down");
 
