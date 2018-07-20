bf3889f0803c5c41c70f18ccc4b016ec18a2f56a
m.giff <m.giff@sterlingbuild.co.uk>
2018-07-20 16:26:38 +0100

    asd

--- game.js
+++ game.js
@@ -1,16 +1,16 @@
-class Player {
+class Player extends Animation{
 	constructor(tex){
-		this.base = new Animation(tex);
+		super(tex);
 		this.weapon = new Animation(tex);
 
-		this.base.addAnimation("idle_right", 0, 0, 100, 100, 100, 100, [{x: 0, t: 10},{x: 100, t: 10},{x: 200, t: 10},{x: 300, t: 10},{x: 400, t: 10},{x: 500, t: 10}]);
-		this.base.addAnimation("idle_left", 0, 0, 100, 100, 100, 100, [{x: 0, t: 10},{x: 100, t: 10},{x: 200, t: 10},{x: 300, t: 10},{x: 400, t: 10},{x: 500, t: 10}]);
+		this.addAnimation("idle_right", 0, 0, 100, 100, 100, 100, [{x: 0, t: 10},{x: 100, t: 10},{x: 200, t: 10},{x: 300, t: 10},{x: 400, t: 10},{x: 500, t: 10}]);
+		this.addAnimation("idle_left", 0, 0, 100, 100, 100, 100, [{x: 0, t: 10},{x: 100, t: 10},{x: 200, t: 10},{x: 300, t: 10},{x: 400, t: 10},{x: 500, t: 10}]);
 
-		this.base.setAnimation("idle_right");
+		this.setAnimation("idle_right");
 	}
 
 	draw(){
-		this.base.draw();
+		this.draw();
 	}
 }
 
