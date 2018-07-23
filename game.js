3eaaf0f3759a0bb7ae4b97bf653d46f3896f6eaa
m.giff <m.giff@sterlingbuild.co.uk>
2018-07-23 09:30:22 +0100

    asd

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 class Player extends Animation{
-	constructor(tex){
-		super(tex);
+	constructor(sprite, weapons){
+		super(sprite);
 		this.addAnimation("idle_right", 0, 0, 120, 120, 120, 120, [{x: 0, t: 5},{x: 120, t: 5},{x: 240, t: 5},{x: 360, t: 5},{x: 480, t: 5},{x: 600, t: 5}]);
 		this.addAnimation("idle_left", 0, 0, -120, 120, 120, 120, [{x: 0, t: 5},{x: 120, t: 5},{x: 240, t: 5},{x: 360, t: 5},{x: 480, t: 5},{x: 600, t: 5}]);
 		this.addAnimation("walk_right", 0, 1, 120, 120, 120, 120, [{x: 0, t: 5},{x: 120, t: 5},{x: 240, t: 5},{x: 360, t: 5},{x: 480, t: 5},{x: 600, t: 5}]);
@@ -9,16 +9,21 @@ class Player extends Animation{
 		this.addAnimation("attackIdle_left", 0, 4, -120, 120, 120, 120, [{x: 0, t: 5},{x: 120, t: 5},{x: 240, t: 5},{x: 360, t: 5},{x: 480, t: 5},{x: 600, t: 5}]);
 		this.setAnimation("attackIdle_right");
 
-		this.weapon = new Animation(tex);
-		this.weapon.addAnimation("gunIdle_right", 0, 16, 60, 60, 60, 60, [{x: 0, t: 50}]);
-		this.weapon.addAnimation("gunShoot_right", 0, 17, 60, 60, 60, 60, [{x: 0, t: 50}]);
-		this.weapon.setAnimation("gunIdle_right");
-		this.weapon.transform.setPosition(45, -5);
+		this.weapon = new Weapon(weapons);
 	}
 
 	draw(camera){
 		super.draw(camera);
 		this.weapon.draw(camera);
+	}
+}
+
+class Weapon extends Animation{
+	constructor(tex){
+		super(tex);
+		this.weapon.addAnimation("gunIdle_right", 0, 16, 60, 60, 60, 60, [{x: 0, t: 50}]);
+		this.weapon.addAnimation("gunShoot_right", 0, 17, 60, 60, 60, 60, [{x: 0, t: 50}]);
+		this.weapon.setAnimation("gunIdle_right");
 	}
 }
 
@@ -36,7 +41,7 @@ requestAnimationFrame(run);
 function run(t) {
 	if (Keyboard.getKey('left') && !Keyboard.getKey('right'))
 		sprite.setAnimation("attackIdle_left");
-		if (Keyboard.getKey('right') && !Keyboard.getKey('left'))
+	if (Keyboard.getKey('right') && !Keyboard.getKey('left'))
 		sprite.setAnimation("attackIdle_right");
 
 	game.clear();
