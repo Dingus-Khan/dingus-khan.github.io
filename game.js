890d738169ca3a57434fa00d8e276060f221406e
m.giff <m.giff@sterlingbuild.co.uk>
2018-08-09 15:49:54 +0100

    asd

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 var game = new Window(800, 600, 800, 600);
 
-var sprite = new Sprite("tileset.png", 0, 0, 120, 120, 0, 0, 120, 120, 1, 1, 1);
+var bullet = new Sprite("proj.png", 0, 0, 120, 120, 0, 0, 120, 120, 1, 1, 1);
 
 requestAnimationFrame(run);
 function run(t) {
 	game.clear();
-	game.draw(sprite);
+	game.draw(bullet);
 	requestAnimationFrame(run);
 }
