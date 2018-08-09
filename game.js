f4bcfb2ed3aa952039028c1060b82b3357bdb19a
m.giff <m.giff@sterlingbuild.co.uk>
2018-08-09 16:00:02 +0100

    asd

--- game.js
+++ game.js
@@ -7,8 +7,15 @@ var bullet3 = new Sprite("proj.png", 0, 0, 60, 60, 0, 0, 20, 20, 1, 1, 1);
 bullet.transform.move(-30, 0);
 bullet3.transform.move(30, 0);
 
+Keyboard.registerKey('space', 32);
+
 requestAnimationFrame(run);
 function run(t) {
+
+	if (Keyboard.wasKeyPressed("space"))
+		console.log("shoot");
+
+	Keyboard.update();
 	game.clear();
 	game.draw(bullet);
 	game.draw(bullet2);
