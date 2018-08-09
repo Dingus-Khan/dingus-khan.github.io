f21ab95bcbfe99ba97d6497893fe0fb4e97632a2
m.giff <m.giff@sterlingbuild.co.uk>
2018-08-09 15:58:20 +0100

    asd

--- game.js
+++ game.js
@@ -4,9 +4,14 @@ var bullet = new Sprite("proj.png", 0, 0, 20, 20, 0, 0, 20, 20, 1, 1, 1);
 var bullet2 = new Sprite("proj.png", 0, 0, 40, 40, 0, 0, 20, 20, 1, 1, 1);
 var bullet3 = new Sprite("proj.png", 0, 0, 60, 60, 0, 0, 20, 20, 1, 1, 1);
 
+bullet.transform.move(-30, 0);
+bullet3.transform.move(30, 0);
+
 requestAnimationFrame(run);
 function run(t) {
 	game.clear();
 	game.draw(bullet);
+	game.draw(bullet2);
+	game.draw(bullet3);
 	requestAnimationFrame(run);
 }
