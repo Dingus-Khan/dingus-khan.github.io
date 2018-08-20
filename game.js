e74567f1dc1dff804d910b4cc0f1b0589cc98c81
m.giff <m.giff@sterlingbuild.co.uk>
2018-08-20 15:57:38 +0100

    asd

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ var game = new Window(800, 600, 800, 600);
 
 var sprite = new Sprite("circle.png", 0, 0, 100, 100, 0, 0, 100, 100, 1, 1, 1);
 
+sprite.transform.x = 100;
+
 requestAnimationFrame(run);
 function run(t) {
 	Keyboard.update();
