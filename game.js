30ef3d092d25a0b2be8008a6c862c6044a932ab9
m.giff <m.giff@sterlingbuild.co.uk>
2018-08-23 17:27:29 +0100

    asd

--- game.js
+++ game.js
@@ -1,6 +1,40 @@
 var game = new Window(800, 600, 800, 600);
 var sprite = new Sprite("circle.png", 0, 0, 100, 100, 0, 0, 100, 100, 0, 1, 1);
 
+/**********************************\
+\**********************************/
+
+class EntityHandler {
+	constructor(){
+		this.entities = [];
+		this.entityId = 1;
+	}
+
+	getEntity(id){
+		return this.entities.find(function(entity){ return entity.id == id; });
+	}
+
+	registerEntity(entity){
+		entity.id = this.entityId;
+		this.entityId++;
+	}
+
+	unregisterEntity(id){
+		var index = this.entities.findIndex(function(entity){return entity.id == id;});
+		this.entities.splice(index, 1).id = undefined;
+	}
+
+	getNearbyEntities(id, range){
+		var item = this.entities.findIndex(function(elem){ return elem.id == id; });
+		return this.entities.filter(function(entity){
+			var dx = entity.transform.x - this.entities[item].transform.x;
+			var dy = entity.transform.y - this.entities[item].transform.y;
+
+			return Math.abs()
+		});
+	}
+}
+
 requestAnimationFrame(run);
 function run(t) {
 	Keyboard.update();
