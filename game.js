efee1bd87e4f22de47e928ba9419b4e799d9291d
m.giff <m.giff@sterlingbuild.co.uk>
2018-08-28 15:50:05 +0100

    asd

--- game.js
+++ game.js
@@ -4,25 +4,26 @@ Keyboard.registerKey('space', 32);
 class EntityHandler {
 	constructor(){
 		this.entities = {};
-		this.renderables = {};
+		this.renderables = [];
 		this.entityId = 1;
 	}
 
 	registerEntity(entity){
 		this.entities[this.entityId] = entity;
 		entity.id = this.entityId;
-		entity.components = {};
-		entity.addComponent = function(component){ this.components[component.name] = component; }
-		entity.removeComponent = function(component){ if (component.name !== undefined){ component = component.name } this.component[component] = undefined; }
 		this.entityId++;
+
+		if (entity.draw !== undefined)
+			this.renderables.push(entity);
 	}
 
 	unregisterEntity(id){
 		this.entities[id].entity.id = undefined;
-		entity.components = undefined;
-		entity.addComponent = undefined;
-		entity.removeComponent = undefined;
 		this.entities[id] = undefined;
+		var ren = this.renderables.findIndex(function(elem){ return elem.id == id; });
+		if (ren !== undefined){
+			this.renderables.splice(ren, 1);
+		}
 	}
 
 	getEntity(id){
@@ -51,10 +52,8 @@ requestAnimationFrame(run);
 function run(t) {
 	game.t = t - this.pastTime;
 	this.pastTime = t;
-	console.log(game.t);
 
 	Keyboard.update();
 	game.clear();
-
 	requestAnimationFrame(run);
 }
