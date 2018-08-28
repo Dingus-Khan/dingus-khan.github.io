3b8a1f26b90273d6c60e689290b011228ac95f1a
m.giff <m.giff@sterlingbuild.co.uk>
2018-08-28 16:56:35 +0100

    asd

--- game.js
+++ game.js
@@ -39,7 +39,7 @@ class EntityHandler {
 
 ////////////////////////////////////////
 var entities = new EntityHandler();
-
+ 
 var sprite = new Sprite("circle.png", 0, 0, 100, 100, 0, 0, 100, 100, 0, 1, 1);
 //entities.registerEntity(sprite);
 
