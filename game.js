222667eb9d32137be4298a91df12a2d2bf8cbc98
m.giff <m.giff@sterlingbuild.co.uk>
2018-08-30 16:19:39 +0100

    frame rate independant

--- game.js
+++ game.js
@@ -35,21 +35,25 @@ class EntityHandler {
 var entities = new EntityHandler();
 
 var sprites = new SpriteBatch(cowSheetUri.uri);
-sprites.addSprite(-10, -10, 110, 110, 0, 0, 110, 110, 1, 0.5, 0.5);
+sprites.addSprite(0, 0, 110, 110, 0, 0, 110, 110, 1, 0.5, 0.5);
 var a = sprites.sprites[0];
+var vel = {x: 0, y: 0};
 
+game.t = 0;
+game.pastTime = 0;
 
 requestAnimationFrame(run);
 function run(t) {
-	game.t = t - this.pastTime;
-	this.pastTime = t;
+	game.t = t - game.pastTime;
+	game.pastTime = t;
 
 	if (Keyboard.getKey('space'))
 		sprites.addSprite(Math.random() * 800, Math.random() * 600, 110, 110, 0, 0, 110, 110, Math.random(), Math.random(), Math.random());
 
-	a.x = lerp(a.x, Mouse.x, 0.01);
-	a.y = lerp(a.y, Mouse.y, 0.01);
+	vel.x = a.x - lerp(a.x, Mouse.x, game.t / 1000);
+	vel.y = a.y - lerp(a.y, Mouse.y, game.t / 1000);
 
+	a.x -= vel.x; a.y -= vel.y;
 	game.clear();
 	game.draw(sprites);
 	requestAnimationFrame(run);
